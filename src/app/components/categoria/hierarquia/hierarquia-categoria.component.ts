a715fe5894d9fb31121d14914b65d7c72c915085
Diego Jesus Cordeiro <diego.cordeiro@unicid.edu.br>
2019-08-19 18:42:25 -0300

    Adicionar função hierarquia

--- src/app/components/categoria/hierarquia/hierarquia-categoria.component.ts
+++ src/app/components/categoria/hierarquia/hierarquia-categoria.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Categoria, CategoriaService } from 'src/app/share';
 
 @Component({
   selector: 'app-hierarquia-categoria',
   templateUrl: './hierarquia-categoria.component.html',
   styleUrls: ['./hierarquia-categoria.component.css']
 })
-export class HierarquiaCategoriaComponent implements OnInit {
+export class HierarquiaCategoriaComponent implements OnInit{
 
-  constructor() { }
+  @Input()
+  public id: number;
+
+  @Input()
+  public nomeCategoriaPai: string;
+
+  @Input()
+  public categorias: Categoria[];
+
+  constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit() {
   }
-
 }
--- src/app/components/categoria/listar/listar-categoria.component.ts
+++ src/app/components/categoria/listar/listar-categoria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { CategoriaService, Categoria } from 'src/app/share';
+import { CategoriaService, Categoria, NotificationType } from 'src/app/share';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-listar-categoria',
@@ -14,11 +15,13 @@ export class ListarCategoriaComponent implements OnInit {
   public categorias: Categoria[];
   public categoriasFilhas: Categoria[];
   public exibeEditar: boolean;
+  public nomeCategoriaPai: string;
 
-  constructor(private categoriaService: CategoriaService) { }
+  constructor(private notifierService: NotifierService,
+              private categoriaService: CategoriaService) { }
 
   ngOnInit() {
-
+    this.nomeCategoriaPai = '';
     this.idSelected = 0;
     this.check = false;
     this.exibeEditar = false;
@@ -42,13 +45,16 @@ export class ListarCategoriaComponent implements OnInit {
     }
   }
 
-  selectCheckBox(id: number): void {
+  selectCheckBox(id: number, nome: string): void {
 
     if ( id !== this.idSelected ) {
-
+      this.categoriasFilhas = [];
+      this.validateIfHaveHierarchy(id);
+      this.nomeCategoriaPai = nome;
       this.idSelected = id;
     } else {
 
+      this.nomeCategoriaPai = '';
       this.idSelected = 0;
     }
   }
@@ -66,6 +72,29 @@ export class ListarCategoriaComponent implements OnInit {
 
   habilitaBtns(): boolean {
 
+    return this.idSelected > 0;
+  }
+
+  habilitaBtnVisualizarHistorico(): boolean {
+
+    return (this.categoriasFilhas !== undefined && this.categoriasFilhas.length > 0 && this.idSelected > 0);
+  }
+
+  habilitaMsgQuantidade(): boolean {
+
     return (this.categorias !== undefined && this.categorias.length > 0);
+  }
+
+  validateIfHaveHierarchy(id: number): void {
+      this.categoriaService.findByCategoriaPai(id).subscribe(data => {
+        this.categoriasFilhas = data;
+      });
+
+      if ((this.categoriasFilhas !== undefined && this.categoriasFilhas.length > 0)) {
+        this.notifierService.show({
+          type: NotificationType.SHOW_INFO,
+          message: 'Categoria possuí filhos.',
+        });
+      }
   }
 }
